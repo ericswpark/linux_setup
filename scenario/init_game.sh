73fceb253eef836aaeaf7ea517d007da6ee70b4a
Eric Park <ideamaneric@gmail.com>
2019-11-27 11:41:25 +0800

    Add Factorio to game setup script

--- scenario/init_game.sh
+++ scenario/init_game.sh
@@ -3,6 +3,7 @@
 # This script sets up a game server from scratch.
 # Will go through Factorio setup and Minecraft setup
 
-# REPLACE THIS LINE WITH FACTORIO SETUP
+# Factorio setup (comment this if you don't want Factorio)
+curl -o- https://raw.githubusercontent.com/ideaman924/linux_setup/master/scenario/init_factorio.sh | bash
 
 # REPLACE THIS LINE WITH MINECRAFT SETUP
