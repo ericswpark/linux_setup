40556f7dc9eefe2f8d5818dc2332d36521c9e477
Eric Park <ideamaneric@gmail.com>
2019-11-27 11:37:52 +0800

    Game script template
    Not complete yet!

---
+++ scenario/init_game.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script sets up a game server from scratch.
+# Will go through Factorio setup and Minecraft setup
+
+# REPLACE THIS LINE WITH FACTORIO SETUP
+
+# REPLACE THIS LINE WITH MINECRAFT SETUP
