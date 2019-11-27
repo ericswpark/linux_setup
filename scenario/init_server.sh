59db06bbe2243a47e33b9e3ab548d90221c34dec
Eric Park <ideamaneric@gmail.com>
2019-11-27 11:37:41 +0800

    Initial server setup script

---
+++ scenario/init_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Initial server setup for Debian 10
+
+read -p "Enter username: " username
+
+adduser $username
+apt update
+apt upgrade
+apt install -y sudo
+usermod -aG sudo $username
+apt install -y tmux htop
+cp -r ~/.ssh /home/$username
+chown -R $username:$username /home/$username/.ssh
+
+echo "Initial set up complete."
