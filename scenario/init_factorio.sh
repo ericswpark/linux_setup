52db6e024a5cf28a45692a796aab2856e551c7a9
Eric Park <ideamaneric@gmail.com>
2019-12-03 00:38:22 +0800

    init_factorio.sh: Comments

--- scenario/init_factorio.sh
+++ scenario/init_factorio.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# Ask what Factorio version to install
 read -p "Enter Factorio version: " factorio_version
 
+# Required for extracting Factorio's curious tar format
 sudo apt install xz-utils
+
+# Fetch and unpack headless server
 wget -O factorio.tar https://www.factorio.com/get-download/$factorio_version/headless/linux64
 tar -xvf factorio.tar
+
+# Enter directory
 cd factorio
 
-# Start-up script
+# Start-up script generation
 echo "./bin/x64/factorio --start-server-load-latest --server-settings ./config/server-settings.json" > start.sh
 
-# Backup script
+# Backup script generation
 echo "#!/bin/bash
 
 date=\$(date '+%Y%m%d')
@@ -31,6 +37,7 @@ chmod +x start.sh
 chmod +x backup.sh
 cd
 
+# Show result to user
 echo "Factorio setup done. Please put in your save files in the factorio/ directory."
 echo "You can start the server by running the start.sh file."
 echo "To back up your server, run backup.sh."
