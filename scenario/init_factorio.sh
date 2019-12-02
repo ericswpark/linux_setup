534e22c792c9fd1bd8f865fef5dde17403127941
Eric Park <ideamaneric@gmail.com>
2019-12-03 00:29:33 +0800

    Add backup script

--- scenario/init_factorio.sh
+++ scenario/init_factorio.sh
@@ -6,9 +6,31 @@ sudo apt install xz-utils
 wget -O factorio.tar https://www.factorio.com/get-download/$factorio_version/headless/linux64
 tar -xvf factorio.tar
 cd factorio
+
+# Start-up script
 echo "./bin/x64/factorio --start-server-load-latest --server-settings ./config/server-settings.json" > start.sh
+
+# Backup script
+echo "#!/bin/bash
+
+date=$(date '+%Y%m%d')
+
+tar -czvf \"$date\".tar.gz \
+    saves/ \
+    config/ \
+    achievements.dat \
+    config-path.cfg \
+    server-adminlist.json \
+    player-data.json \
+    start.sh
+
+echo \"Backup created!\"" > backup.sh
+
+# Give executable permissions for both files
 chmod +x start.sh
+chmod +x backup.sh
 cd
 
 echo "Factorio setup done. Please put in your save files in the factorio/ directory."
 echo "You can start the server by running the start.sh file."
+echo "To back up your server, run backup.sh."
