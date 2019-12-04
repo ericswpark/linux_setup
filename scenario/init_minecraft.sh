68a120a7ebad98c655a952d8259dc55eb0081e3f
Eric Park <ideamaneric@gmail.com>
2019-12-04 09:48:37 +0800

    Add different Minecraft server distributions
    Apparently PaperMC is better...?

--- scenario/init_minecraft.sh
+++ scenario/init_minecraft.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Pre-setup questions
-echo "Follow this URL to get the server package URL: https://www.minecraft.net/en-us/download/server/"
+echo "Downloading server component..."
+echo "Note that you can choose between vanilla, Paper, Bukkit, Spigot, and other Minecraft Servers."
+echo "To download vanilla, follow this URL: https://www.minecraft.net/en-us/download/server/"
+echo "To download PaperMC, follow this URL: https://papermc.io/downloads"
 read -p "Enter Minecraft server package URL: " minecraft_url
 read -p "Enter minimum RAM in MB (256 recommended, 32 for low RAM): " ram_minimum
 read -p "Enter maximum RAM in MB (512 recommended, 256 for low RAM): " ram_maximum
