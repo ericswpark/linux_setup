5efd75f579d0a7d3b54c6b0b0724e5c0be499f38
Eric Park <me@ericswpark.com>
2023-03-02 19:24:27 +0900

    Add EndeavourOS setup script

---
+++ setup_endeavouros_kde.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install ffmpegthumbs for video thumbnail previews in Dolphin
+# (Don't forget to enable them in settings!)
+sudo pacman -S ffmpegthumbs
+
+# Install KDE Plasma addons (for window switchers, etc.)
+sudo pacman -S kdeplasma-addons
