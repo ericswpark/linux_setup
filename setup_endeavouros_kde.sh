b6c5df0d3f4ebbc71c5fc59173112777ec69a818
Eric Park <me@ericswpark.com>
2023-03-03 12:04:35 +0900

    Update setup_endeavouros_kde.sh

--- setup_endeavouros_kde.sh
+++ setup_endeavouros_kde.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install basic things
+sudo pacman -S htop vim
+
 # Install ffmpegthumbs for video thumbnail previews in Dolphin
 # (Don't forget to enable them in settings!)
 sudo pacman -S ffmpegthumbs
