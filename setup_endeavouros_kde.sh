9620f8ab2eda9e6f16333977e537bc370493a6c3
Eric Park <me@ericswpark.com>
2023-03-03 12:07:03 +0900

    Update setup_endeavouros_kde.sh
    Only install when it's missing, and don't prompt for confirmation

--- setup_endeavouros_kde.sh
+++ setup_endeavouros_kde.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install basic things
-sudo pacman -S htop vim
+sudo pacman -S --noconfirm --needed htop vim
 
 # Install ffmpegthumbs for video thumbnail previews in Dolphin
 # (Don't forget to enable them in settings!)
-sudo pacman -S ffmpegthumbs
+sudo pacman -S --noconfirm --needed ffmpegthumbs
 
 # Install KDE Plasma addons (for window switchers, etc.)
-sudo pacman -S kdeplasma-addons
+sudo pacman -S --noconfirm --needed kdeplasma-addons
