e69a52809810608f1cc816eeb3e79dd319c50513
Eric Park <me@ericswpark.com>
2024-06-05 22:48:57 +0900

    endeavouros: remove kde extras
    Already installed by default on newer EOS releases

--- setup_endeavouros_kde.sh
+++ setup_endeavouros_kde.sh
@@ -3,9 +3,3 @@
 # Install basic things
 sudo pacman -S --noconfirm --needed btop vim mpv
 
-# Install ffmpegthumbs for video thumbnail previews in Dolphin
-# (Don't forget to enable them in settings!)
-sudo pacman -S --noconfirm --needed ffmpegthumbs
-
-# Install KDE Plasma addons (for window switchers, etc.)
-sudo pacman -S --noconfirm --needed kdeplasma-addons
